fce618381e4142458087e50437c11c1588018163
NikoRozo <niko.rozo92@outlook.com>
2019-04-19 15:49:51 -0500

    Test Seriales y modelo agent

--- platziverse-db/tests/anget-test.js
+++ platziverse-db/tests/anget-test.js
@@ -1,22 +1,56 @@
 'use strict'
 
 const test = require('ava')
+const sinon = require('sinon')
+const proxyquire = require('proxyquire')
+
+const AgentFixtures = require('./fixtures/agent');
 
 let config = {
-    logging: function () { }
+  logging: function () { }
 }
 
+let MetricStub = {
+  belongsTo: sinon.spy()
+}
+
+let single = Object.assign({}, AgentFixtures.single)
+let id = 1
+let AgentStub = null
 let db = null
+let sandbox = null
 
 test.beforeEach(async () => {
-  const setupDataBase = require('../index.')
+  sandbox = sinon.createSandbox()
+  AgentStub = {
+    hasMany: sandbox.spy()
+  }
+
+  const setupDataBase = proxyquire('../index.', {
+    './models/agent': () => AgentStub,
+    './models/metric': () => MetricStub
+  })
+
   db = await setupDataBase(config)
+})
+
+test.afterEach(() => {
+  sandbox && sandbox.restore()
 })
 
 test('Agent', t => {
   t.truthy(db.Agent, 'Agent service should exist')
 })
 
-test('Metric', t => {
-  t.truthy(db.Metric, 'Metric service should exist')
+test.serial('Setup', t => {
+  t.truthy(AgentStub.hasMany.called, 'AgentModel.HhasMeny was executed')
+  t.truthy(AgentStub.hasMany.calledWith(MetricStub), 'Argument should be Metric Model')
+  t.truthy(MetricStub.belongsTo.called, 'AgentModel.HhasMeny was executed')
+  t.truthy(MetricStub.belongsTo.calledWith(AgentStub), 'Argument should be Agent Model')
+})
+
+test.serial('Agent#findById', async t => {
+    let agent = await db.Agent.findById(id)
+
+    t.deepEqual(agent, AgentFixtures.byId(id), 'should be the same')
 })
