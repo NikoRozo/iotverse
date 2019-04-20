c231f810f309274eeb11d0ce4be3052975a92770
NikoRozo <niko.rozo92@outlook.com>
2019-04-20 18:49:12 -0500

    Implementación de broadcast

--- iotverse-agent/index.js
+++ iotverse-agent/index.js
@@ -1,24 +1,73 @@
 'use strict'
 
+const debug = require('debug')('iotverse:agent')
+const mqtt = require('mqtt')
+const defaults = require('defaults')
+const uuid = require('uuid')
 const EventEmitter = require('events')
+
+const { parsePayload } = require('./utils')
+
+const options = {
+    name: 'untitled',
+    username: 'platzi',
+    interval: 5000,
+    mqtt: {
+      host: 'mqtt://localhost'
+    }
+  }
 
 class IoTVerseAgent extends EventEmitter {
   constructor (opts) {
     super()
 
-    this._options = opts
+    this._options = defaults(opts, options)
     this._started = false
     this._timer = null
+    this._client = null
+    this,_agentId = null
   }
 
   connect () {
     if (!this._timer) {
-      this._started = true
-      this.emit('disconnected')
       const opts = this._options
-      this._timer = setInterval(() => {
-        this.emit('agent/message', 'this is message')
-      }, opts.interval)
+      this._client = mqtt.connect(opts.mqtt.host)
+      this._started = true
+
+      this._client.subscribe('agent/connected')
+      this._client.subscribe('agent/message')
+      this._client.subscribe('agent/disconnected')
+
+      this._client.on('connect', () => {
+        this._agentId = uuid.v4()
+        this.emit('connected', this._agentId)
+
+        this._timer = setInterval(() => {
+          this.emit('agent/message', 'this is message')
+        }, opts.interval)
+      })
+
+      this._client.on('agent/message', (topic, payload) => {
+        payload = parsePayload(payload)
+
+        let broadcast = null
+        switch (topic) {
+            case 'agent/connected':
+            case 'agent/message':
+            case 'agent/disconnected':
+                broadcast = payload && payload.agent && payload.agent.uuid !== this._agentId
+                break;
+        }
+
+        if (broadcast) {
+            this.emit(topic, payload)
+        }
+      })
+
+      this._client.on('error', () => {
+        this.disconnect()
+      })
+
     }
   }
 
