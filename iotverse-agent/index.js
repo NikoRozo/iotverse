6c2f12479efa28c47c1efbc84fee01f273a7b9d4
NikoRozo <niko.rozo92@outlook.com>
2019-04-20 18:07:55 -0500

    Inicializar el proyecto Agent

---
+++ iotverse-agent/index.js
@@ -0,0 +1,11 @@
+'use strict'
+
+const EventEmitter = require('events')
+
+class IoTVerseAgent extends EventEmitter {
+    constructor() {
+        super()
+    }
+}
+
+module.exports = IoTVerseAgent
