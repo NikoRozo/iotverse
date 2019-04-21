615db8c4dceb63a153d525fc9c9c9613ecf6c286
NikoRozo <niko.rozo92@outlook.com>
2019-04-20 19:16:52 -0500

    Ejemplos de ejecución

--- iotverse-agent/index.js
+++ iotverse-agent/index.js
@@ -11,13 +11,13 @@ const EventEmitter = require('events')
 const { parsePayload } = require('./utils')
 
 const options = {
-    name: 'untitled',
-    username: 'platzi',
-    interval: 5000,
-    mqtt: {
-      host: 'mqtt://localhost'
-    }
+  name: 'untitled',
+  username: 'platzi',
+  interval: 5000,
+  mqtt: {
+    host: 'mqtt://localhost'
   }
+}
 
 class IoTVerseAgent extends EventEmitter {
   constructor (opts) {
@@ -27,86 +27,84 @@ class IoTVerseAgent extends EventEmitter {
     this._started = false
     this._timer = null
     this._client = null
-    this,_agentId = null
+    this._agentId = null
     this._metrics = new Map()
   }
 
   addMetric (type, fn) {
-      this._metrics.set(type, fn)
+    this._metrics.set(type, fn)
   }
 
   removeMetric (type) {
-      this._metrics.delete(type)
+    this._metrics.delete(type)
   }
 
   connect () {
-    if (!this._timer) {
+    if (!this._started) {
       const opts = this._options
       this._client = mqtt.connect(opts.mqtt.host)
       this._started = true
 
-      this._client.subscribe('agent/connected')
       this._client.subscribe('agent/message')
+      this._client.subscribe('agent/connected')
       this._client.subscribe('agent/disconnected')
 
       this._client.on('connect', () => {
         this._agentId = uuid.v4()
+
         this.emit('connected', this._agentId)
 
-        this._timer = setInterval( async () => {
+        this._timer = setInterval(async () => {
           if (this._metrics.size > 0) {
-              let message = {
-                  agent: {
-                      uuid: this._agentId,
-                      username: opts.username,
-                      name: opts.name,
-                      hostname: os.hostname() || 'localhost',
-                      pid: process.pid
-                  },
-                  metrcs: [],
-                  timestamp: new Date().getTime()
-              }
-          }
+            let message = {
+              agent: {
+                uuid: this._agentId,
+                username: opts.username,
+                name: opts.name,
+                hostname: os.hostname() || 'localhost',
+                pid: process.pid
+              },
+              metrics: [],
+              timestamp: new Date().getTime()
+            }
 
-          for (let [metrc, fn] of this._metrics) {
-              if (fn.legnth == 1) {
-                  fn = util.promisify(fn)
+            for (let [ metric, fn ] of this._metrics) {
+              if (fn.length === 1) {
+                fn = util.promisify(fn)
               }
 
-              message.metrcs.push({
-                  type: metric,
-                  value: await Promise.resolve(fn())
+              message.metrics.push({
+                type: metric,
+                value: await Promise.resolve(fn())
               })
+            }
+
+            debug('Sending', message)
+
+            this._client.publish('agent/message', JSON.stringify(message))
+            this.emit('message', message)
           }
-
-          debug('sending', message)
-
-          this._client.publish('agent/message', JSON.stringify(message))
-          this.emit('message', message)
         }, opts.interval)
       })
 
-      this._client.on('agent/message', (topic, payload) => {
+      this._client.on('message', (topic, payload) => {
         payload = parsePayload(payload)
 
-        let broadcast = null
+        let broadcast = false
         switch (topic) {
-            case 'agent/connected':
-            case 'agent/message':
-            case 'agent/disconnected':
-                broadcast = payload && payload.agent && payload.agent.uuid !== this._agentId
-                break;
+          case 'agent/connected':
+          case 'agent/disconnected':
+          case 'agent/message':
+            broadcast = payload && payload.agent && payload.agent.uuid !== this._agentId
+            break
         }
 
         if (broadcast) {
-            this.emit(topic, payload)
+          this.emit(topic, payload)
         }
       })
 
-      this._client.on('error', () => {
-        this.disconnect()
-      })
-
+      this._client.on('error', () => this.disconnect())
     }
   }
 
