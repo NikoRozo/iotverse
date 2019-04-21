b95c0c69378be8094d238dc71946c26c610c7566
NikoRozo <niko.rozo92@outlook.com>
2019-04-20 21:54:28 -0500

    Implementación Servidor Estatico

---
+++ iotverse-web/server.js
@@ -0,0 +1,37 @@
+'use strict'
+
+const debug = require('debug')('iotverse:web')
+const path = require('path')
+const http = require('http')
+const express = require('express')
+const chalk = require('chalk')
+
+const port = process.env.PORT || 8080
+const app = express()
+const server = http.createServer(app)
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+// Express Error Handler
+app.use((err, req, res, next) => {
+  debug(`Error: ${err.message}`)
+
+  if (err.message.match(/not found/)) {
+    return res.status(404).send({ error: err.message })
+  }
+
+  res.status(500).send({ error: err.message })
+})
+
+function handleFatalError (err) {
+  console.error(`${chalk.red('[fatal error]')} ${err.message}`)
+  console.error(err.stack)
+  process.exit(1)
+}
+
+process.on('uncaughtException', handleFatalError)
+process.on('unhandledRejection', handleFatalError)
+
+server.listen(port, () => {
+  console.log(`${chalk.green('[iotverse-web]')} server listening on port ${port}`)
+})
